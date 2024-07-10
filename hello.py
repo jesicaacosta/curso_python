9481cebb347f7ce52caa99c6b6bec9fbdfab66a6
jesicadaiana <ajesicadaiana@gmail.com>
2024-07-09 22:19:06 -0300

    Operadores

--- hello.py
+++ hello.py
@@ -36,3 +36,48 @@ print('Tu edad es ',agee)
 
 #operadores 
 
+### Operadores Aritméticos ###
+
+# Operaciones con enteros
+print(10 // 3)
+print(2 ** 3)
+print(2 ** 3 + 3 - 7 / 1 // 4)
+
+#Operaciones con cadenas de texto
+print("Hola " + str(5))
+
+# Operaciones mixtas
+print("Hola " * 5)
+print("Hola " * (2 ** 3))
+
+my_float = 2.5 * 2
+print("Hola " * int(my_float))
+
+### Operadores Comparativos ###
+
+print("Operaciones con enteros")
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(4 <= 4)
+print(3 == 4)
+print(3 != 4)
+
+print(" Operaciones con cadenas de texto")
+print("Hola" > "Python")
+print("Hola" < "Python")
+print("aaaa" >= "abaa")  # Ordenación alfabética por ASCII
+print(len("aaaa") >= len("abaa"))  # Cuenta caracteres
+print("Hola" <= "Python")
+print("Hola" == "Hola")
+print("Hola" != "Python")
+
+### Operadores Lógicos ###
+
+print("Basada en el Álgebra de Boole https://es.wikipedia.org/wiki/%C3%81lgebra_de_Boole ")
+print(3 > 4 and "Hola" > "Python")
+print(3 > 4 or "Hola" > "Python")
+print(3 < 4 and "Hola" < "Python")
+print(3 < 4 or "Hola" > "Python")
+print(3 < 4 or ("Hola" > "Python" and 4 == 4))
+print(not (3 > 4))
